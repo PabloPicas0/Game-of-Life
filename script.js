bfd926ee8471f2984b209f9fb3743e6c77c57e13
PabloPicas0 <pawel.fe@gmail.com>
2023-11-21 18:56:39 +0100

    feat: add function for grid generation

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const body = document.querySelector("body")
-console.log(body)
+const board = document.querySelector(".board");
+
+const createGrid = () => {
+  const columns = 48;
+  const rows = 28;
+  let cellCount = 0;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const div = document.createElement("div");
+      div.id = cellCount;
+      div.classList.add("cell");
+
+      board.appendChild(div);
+
+      cellCount++;
+    }
+  }
+};
+
+createGrid();
