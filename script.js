2590b5d4462aab76e04a9b9338d8ef10c62ed817
PabloPicas0 <pawel.fe@gmail.com>
2023-11-22 23:55:43 +0100

    feat: add function for counting cell neighbours

--- script.js
+++ script.js
@@ -29,3 +29,53 @@ for (let i = 0; i < rows; i++) {
     cellCount++;
   }
 }
+
+// TODO: DRY this function
+const getCellNeighbours = (grid, i, j) => {
+  let neighbours = 0;
+
+  // check horizontal cells
+  if (j + 1 < 48 && grid[i][j + 1] === 1) {
+    neighbours++;
+  }
+  if (j - 1 >= 0 && grid[i][j - 1] === 1) {
+    neighbours++;
+  }
+
+  // check vertical cells
+  if (i + 1 < 28 && grid[i + 1][j] === 1) {
+    neighbours++;
+  }
+  if (i - 1 >= 0 && grid[i - 1][j] === 1) {
+    neighbours++;
+  }
+
+  // check lower diagonal cells
+  if (i + 1 < 28 && j + 1 <= 48 && grid[i + 1][j + 1] === 1) {
+    neighbours++;
+  }
+  if (i + 1 < 28 && j - 1 >= 0 && grid[i + 1][j - 1] === 1) {
+    neighbours++;
+  }
+
+  // check upper diagonal cells
+  if (i - 1 >= 0 && j + 1 <= 48 && grid[i - 1][j + 1] === 1) {
+    neighbours++;
+  }
+  if (i - 1 >= 0 && j - 1 >= 0 && grid[i - 1][j - 1] === 1) {
+    neighbours++;
+  }
+
+  return neighbours;
+};
+
+const nextGeneration = (grid, rows, columns) => {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const cellNeighbours = getCellNeighbours(grid, i, j);
+      console.log(cellNeighbours);
+    }
+  }
+};
+console.log(grid)
+nextGeneration(grid, rows, columns);
