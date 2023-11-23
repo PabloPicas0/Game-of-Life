7f1ec3bf1f715aab0117bebcc89e01698823ed38
PabloPicas0 <pawel.fe@gmail.com>
2023-11-23 19:04:31 +0100

    feat: algorithm works but wrong

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const board = document.querySelector(".board");
+const cellGeneration = document.getElementById("generation");
 
 let grid = [];
-let cellCount = 0;
+let generation = 0;
 const rows = 28;
 const columns = 48;
 
@@ -19,14 +20,12 @@ for (let i = 0; i < rows; i++) {
 for (let i = 0; i < rows; i++) {
   for (let j = 0; j < columns; j++) {
     const div = document.createElement("div");
-    div.id = cellCount;
+    div.id = `${i}-${j}`;
     div.classList.add("cell");
 
-    if (grid[i][j] === 0) div.style.backgroundColor = "green";
+    if (grid[i][j] === 1) div.style.backgroundColor = "green";
 
     board.appendChild(div);
-
-    cellCount++;
   }
 }
 
@@ -51,7 +50,7 @@ const getCellNeighbours = (grid, i, j) => {
   }
 
   // check lower diagonal cells
-  if (i + 1 < 28 && j + 1 <= 48 && grid[i + 1][j + 1] === 1) {
+  if (i + 1 < 28 && j + 1 < 48 && grid[i + 1][j + 1] === 1) {
     neighbours++;
   }
   if (i + 1 < 28 && j - 1 >= 0 && grid[i + 1][j - 1] === 1) {
@@ -59,7 +58,7 @@ const getCellNeighbours = (grid, i, j) => {
   }
 
   // check upper diagonal cells
-  if (i - 1 >= 0 && j + 1 <= 48 && grid[i - 1][j + 1] === 1) {
+  if (i - 1 >= 0 && j + 1 < 48 && grid[i - 1][j + 1] === 1) {
     neighbours++;
   }
   if (i - 1 >= 0 && j - 1 >= 0 && grid[i - 1][j - 1] === 1) {
@@ -73,9 +72,50 @@ const nextGeneration = (grid, rows, columns) => {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       const cellNeighbours = getCellNeighbours(grid, i, j);
-      console.log(cellNeighbours);
+      
+      if (grid[i][j] === 1 && cellNeighbours < 2) {
+        grid[i][j] = 0;
+      } else if (grid[i][j] === 1 && cellNeighbours > 3) {
+        grid[i][j] = 0;
+      } else if (grid[i][j] === 0 && cellNeighbours === 3) {
+        grid[i][j] = 1;
+      } else {
+        grid[i][j] = grid[i][j];
+      }
     }
   }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const isCellAlive = grid[i][j];
+      const cell = document.getElementById(`${i}-${j}`);
+
+      if (isCellAlive === 1) {
+        cell.style.backgroundColor = "green";
+      } else {
+        cell.style.backgroundColor = null;
+      }
+    }
+  }
+
+  generation++;
+  cellGeneration.textContent = `Generation: ${generation}`;
 };
-console.log(grid)
+
+const sleep = async (time) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+};
+
+const redraw = async () => {
+  while (true) {
+    nextGeneration(grid, rows, columns);
+    await sleep(80);
+  }
+};
+
 nextGeneration(grid, rows, columns);
+// redraw()
