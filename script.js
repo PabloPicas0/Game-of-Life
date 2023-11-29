5eb274deb68799d32bd297eb0d393bc0aa28f4dd
PabloPicas0 <pawel.fe@gmail.com>
2023-11-29 21:30:32 +0100

    feat: add first patterns

--- script.js
+++ script.js
@@ -147,6 +147,39 @@ patterns.addEventListener("click", () => {
   patternOptions.style.display = isPatternOptionsVisible ? "block" : "none";
 });
 
+gliderGun.addEventListener("click", () => {
+  grid = createPattern(gun, rows, columns);
 
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const isCellAlive = grid[i][j];
+      const cell = document.getElementById(`${i}-${j}`);
+
+      if (isCellAlive === 1) {
+        cell.style.backgroundColor = "#4caf50";
+      } else {
+        cell.style.backgroundColor = null;
+      }
+    }
+  }
+});
+
+smallPulsar.addEventListener("click", () => {
+  grid = createPattern(babyPulsar, rows, columns);
+
+  // TODO: DRY this part of code
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const isCellAlive = grid[i][j];
+      const cell = document.getElementById(`${i}-${j}`);
+
+      if (isCellAlive === 1) {
+        cell.style.backgroundColor = "#4caf50";
+      } else {
+        cell.style.backgroundColor = null;
+      }
+    }
+  }
+});
 
 startGame();
